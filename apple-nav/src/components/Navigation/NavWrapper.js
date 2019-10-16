79602af776f6892765872697b07f61e80ecf11d8
Brad <brad@viziworx.com>
2019-10-15 20:33:20 -0400

    Setup NavWrapper and added svgs

---
+++ apple-nav/src/components/Navigation/NavWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavWrapper = ({children}) => {
+    return (
+        <header className="navigation">
+            {children}
+        </header>
+    )
+}
+
+export default NavWrapper;
